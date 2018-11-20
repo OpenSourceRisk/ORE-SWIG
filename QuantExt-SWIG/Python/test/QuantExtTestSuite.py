a4906cb9ab01fb2bb1e77198fe158efbc8ff91e6
Danilo Delgado <danilo.delgado@quaternionrisk.com>
2018-11-20 18:20:49 +0000

    added ccy basis swap helpers test suit

--- QuantExt-SWIG/Python/test/QuantExtTestSuite.py
+++ QuantExt-SWIG/Python/test/QuantExtTestSuite.py
@@ -14,6 +14,7 @@ from instruments import CommodityForwardTest
 from instruments import PaymentTest
 from instruments import CrossCurrencyFixFloatSwapTest
 from ratehelpers import AverageOISRateHelpersTest
+from ratehelpers import CrossCcyBasisSwapHelperTest
 
 def test():
     import QuantExt
@@ -31,6 +32,7 @@ def test():
     suite.addTest(unittest.makeSuite(PaymentTest, 'test'))
     suite.addTest(unittest.makeSuite(CrossCurrencyFixFloatSwapTest, 'test'))
     suite.addTest(unittest.makeSuite(AverageOISRateHelpersTest, 'test'))
+    suite.addTest(unittest.makeSuite(CrossCcyBasisSwapHelperTest, 'test'))
 
     result = unittest.TextTestRunner(verbosity=2).run(suite)
 
