7bee4182fba81177af7dff131f77065d47f6cc68
rkapl123 <rolandk@kapl.org>
2020-06-26 10:38:56 +0200

    added Python build and VS 2019 to runCmakeVS.cmd

--- runCmakeVS.cmd
+++ runCmakeVS.cmd
@@ -1,4 +1,10 @@
-mkdir builddir
-cmake -G "Visual Studio 15 2017" -A x64 -DSWIG_DIR=C:\dev\swigwin\Lib -DSWIG_EXECUTABLE=C:\dev\swigwin\swig.exe -DORE:PATHNAME=C:\dev\ORE\master -DBOOST_ROOT=C:\dev\boost -S OREAnalytics-SWIG/Java -B builddir
-cmake --build builddir -v
+mkdir build
+rem build Python Swig interfaces VS 2019
+cmake -G "Visual Studio 16 2019" -A x64 -DSWIG_DIR=C:\dev\swigwin\Lib -DSWIG_EXECUTABLE=C:\dev\swigwin\swig.exe -DORE:PATHNAME=C:\dev\ORE\master -DBOOST_ROOT=C:\dev\boost -S OREAnalytics-SWIG/Python -B build
+rem build Java Swig interfaces VS 2019
+rem cmake -G "Visual Studio 16 2019" -A x64 -DSWIG_DIR=C:\dev\swigwin\Lib -DSWIG_EXECUTABLE=C:\dev\swigwin\swig.exe -DORE:PATHNAME=C:\dev\ORE\master -DBOOST_ROOT=C:\dev\boost -S OREAnalytics-SWIG/Java -B build
+rem build Java Swig interfaces VS 2017
+rem cmake -G "Visual Studio 15 2017" -A x64 -DSWIG_DIR=C:\dev\swigwin\Lib -DSWIG_EXECUTABLE=C:\dev\swigwin\swig.exe -DORE:PATHNAME=C:\dev\ORE\master -DBOOST_ROOT=C:\dev\boost -S OREAnalytics-SWIG/Java -B build
+rem build executables
+cmake --build build -v --config Release
 pause
