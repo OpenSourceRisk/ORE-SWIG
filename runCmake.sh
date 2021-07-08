325826387b0718a2a094542f645d496752e4e156
Roland Lichters <roland.lichters@acadia.inc>
2021-07-08 18:00:44 +0200

    cmake command

--- runCmake.sh
+++ runCmake.sh
@@ -1,7 +1,4 @@
 cmake \
--D PYTHON_LIBRARY=/opt/anaconda3/lib/libpython3.8.dylib \
--D PYTHON_INCLUDE_DIR=/opt/anaconda3/include/python3.8 \
--D BOOST_ROOT=$BOOST \
--D BOOST_INCLUDEDIR=$BOOST \
--D BOOST_LIBRARYDIR=$BOOST/stage/lib \
+-D PYTHON_LIBRARY=$PYTHON_LIBRARY \
+-D PYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR \
 -D ORE=$ORE -G Ninja ..
