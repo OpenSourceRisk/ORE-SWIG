5fe2c9a9b725bbf1e3dd200f15f15c56fb9a785d
Eric Ehlers <eric.ehlers@acadia.inc>
2023-06-21 12:20:56 +0100

    increment version number

--- OREAnalytics-SWIG/Python/setup.py
+++ OREAnalytics-SWIG/Python/setup.py
@@ -268,7 +268,7 @@ classifiers = [
 ]
 
 setup(name             = "open-source-risk-engine",
-      version          = "1.8.10",
+      version          = "1.8.10.1",
       description      = "Python bindings for the OREAnalytics library",
       long_description = """
 OREAnalytics (http://opensourcerisk.org/) is a C++ library for financial quantitative
