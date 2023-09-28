4ac8f310ef7a59484e009e00cf3d27aea19ebdbe
Matthias Groncki <matthias.groncki@acadia.inc>
2023-09-28 17:34:36 +0200

    update dockerfiles

---
+++ OREAnalytics-SWIG/Python/Examples/runExamples.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+echo RUN commodityforward.py
+python3 commodityforward.py
+python3 conventions.py
--- OREAnalytics-SWIG/Python/test/testrunner.py
+++ OREAnalytics-SWIG/Python/test/testrunner.py
@@ -6,6 +6,4 @@ import nose
 sys.modules["QuantLib"] = ORE
 
 if __name__=="__main__":
-    folder = sys.argv[1]
-    argv = [__file__, "-w ", folder, '--with-xunit']
-    nose.run(argv=argv)
+    nose.run()
