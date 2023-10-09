54a7ff93744d63ca57bef330dd3472dea14227ad
Matthias Groncki <matthias.groncki@acadia.inc>
2023-10-09 11:02:15 +0200

    QPR-11822 bugfix example shellscript

--- OREAnalytics-SWIG/Python/Examples/runExamples.sh
+++ OREAnalytics-SWIG/Python/Examples/runExamples.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-status = 0
+status=0
+return_code=0
 for file in *.py; do
     if [ -f "$file" ]; then
         echo RUN $file
-        python3 "$file" || status = 1
-        return_code = $?
-        if [$return_code -gt $status]; then
-                status = $return_code
+        python3 "$file" || status=1
+        return_code=$?
+        if [ $return_code -gt $status ]; then
+                status=$return_code
         fi
     fi
 done
